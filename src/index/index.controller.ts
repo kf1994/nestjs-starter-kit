a0b71155d2117bf1a9bb1b395b1b57ceaf2bf979
Mr. KF <mr.kf1994@gmail.com>
2022-11-08 23:27:13 +0500

    feat: index swagger docs updated

--- src/index/index.controller.ts
+++ src/index/index.controller.ts
@@ -2,17 +2,23 @@ import { Controller, Get, Request, Response } from '@nestjs/common';
 import path from 'path';
 
 import { IndexService } from './index.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Index')
 @Controller()
 export class IndexController {
   constructor(private readonly service: IndexService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Return success on home route' })
+  @ApiResponse({ status: 200, description: 'NestJS Backend Boiler Plate' })
   getHello(): string {
     return this.service.getHello();
   }
 
   @Get('/favicon.ico')
+  @ApiOperation({ summary: 'Return favicon.ico' })
+  @ApiResponse({ status: 200, description: 'favicon.ico' })
   getFavicon(@Request() req, @Response() res) {
     return res.sendFile(path.join(__dirname + '../../../assets/favicon.ico'));
   }
