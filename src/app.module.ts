53fff1bc2bd184ee6b3d6750691b30d1e105aaa8
Mr. KF <mr.kf1994@gmail.com>
2022-11-14 21:39:07 +0500

    feat: task module file added and imported in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { CoreModule } from '@core/core.module';
 import { IndexModule } from '@app/index/index.module';
+import { TasksModule } from '@app/tasks/tasks.module';
 
 @Module({
-  imports: [CoreModule, IndexModule],
+  imports: [CoreModule, IndexModule, TasksModule],
   controllers: [],
   providers: [],
 })
