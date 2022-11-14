c500709f22138a86de1f26f5a7031bc35e004ad7
Mr. KF <mr.kf1994@gmail.com>
2022-11-14 22:44:11 +0500

    fix: class validator added in create task dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty({ message: 'title is required' })
+  @IsString({ message: 'title should be a string' })
+  @MaxLength(100)
   title: string;
+
+  @IsNotEmpty({ message: 'description is required' })
+  @IsString({ message: 'description should be a string' })
+  @MaxLength(600)
   description: string;
 }
