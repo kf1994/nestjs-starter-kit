081d4ece78e33af371b9a7526cf75136931f4c47
Mr. KF <mr.kf1994@gmail.com>
2022-11-14 22:41:55 +0500

    fix: swagger controller name fixed

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { TasksService } from '@app/tasks/tasks.service';
 import { ITask } from '@app/tasks/interfaces/task.interface';
 import { CreateTaskDto } from '@app/tasks/dto/create-task.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Task')
 @Controller('tasks')
 export class TasksController {
   constructor(private service: TasksService) {}
